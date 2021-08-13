865fd7bfc263f8f473fee83b3b39681ac5302237
johnyun <johnyun930@gmail.com>
2021-08-13 00:00:43 -0700

    updating our application with reselectm replaced cart selectors with memorized selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {ReactComponent as ShoppingIcon} from '../../assests/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden})=>(
+const CartIcon = ({toggleCartHidden,itemCount})=>(
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 
 const mapDispatchToPros = dispatch => ({
     toggleCartHidden: ()=> dispatch(toggleCartHidden())
-})
+});
 
-export default connect(null,mapDispatchToPros)(CartIcon);
+const mapStateToProps = (state) =>({
+    itemCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps,mapDispatchToPros)(CartIcon);
