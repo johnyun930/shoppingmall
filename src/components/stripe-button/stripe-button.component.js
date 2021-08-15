f75719f0d0330fe1078f07f8bae061581c3fbe1f
johnyun <johnyun930@gmail.com>
2021-08-15 00:30:39 -0700

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price})=>{
+    const priceForStripe = price*100;
+    const publishableKey = "pk_test_51JOdCtEk0bZhl4oBjwS5tnVs8nbCMKwptg84pybxuwzR19vy87XuZpgxQWKFkoCAIQxwIYEBNpf2GWEm8XAYnUYt00v5YQGWhE"
+
+    const onToken = token =>{
+        console.log(token);
+        alert("Payment Successful");
+    }
+
+    return(
+        <StripeCheckout 
+        label="Pay Now" 
+        ame="John's Secret" 
+        billingAddress="Hello" 
+        shippingAddress="world" 
+        image="https://svgshare.com/i/CUz.svg" 
+        description={`Your total is $${price}`}
+        panelLabel="pay now"
+        onToken={onToken}
+        stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
